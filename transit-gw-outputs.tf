307bf50c8676c711f1ccc5501a63c7d903ca2ce6
Cristian Beraha <cristian@cloudops.works>
2024-06-07 13:50:43 -0300

    fix: references

--- transit-gw-outputs.tf
+++ transit-gw-outputs.tf
@@ -18,10 +18,10 @@ output "transit_gateway" {
     vpc_attachments_ids         = module.transit_gateway.ec2_transit_gateway_vpc_attachment_ids
     ram_share_id                = module.transit_gateway.ram_resource_share_id
     default = {
-      association_route_table_id = ec2_transit_gateway_association_default_route_table_id
-      propagation_route_table_id = ec2_transit_gateway_propagation_default_route_table_id
-      association_route_table    = ec2_transit_gateway_route_table_default_association_route_table
-      propagation_route_table    = ec2_transit_gateway_route_table_default_propagation_route_table
+      association_route_table_id = module.transit_gatewayec2_transit_gateway_association_default_route_table_id
+      propagation_route_table_id = module.transit_gatewayec2_transit_gateway_propagation_default_route_table_id
+      association_route_table    = module.transit_gatewayec2_transit_gateway_route_table_default_association_route_table
+      propagation_route_table    = module.transit_gatewayec2_transit_gateway_route_table_default_propagation_route_table
 
     }
   }
