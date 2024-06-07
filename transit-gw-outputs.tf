48082f670efed6da1dd8b2274e80bb14a910807b
Cristian Beraha <cristian@cloudops.works>
2024-06-07 12:26:27 -0300

    chore: be aware of nullable values

--- transit-gw-outputs.tf
+++ transit-gw-outputs.tf
@@ -8,13 +8,14 @@ output "transit_gateway" {
   value = {
     id                          = module.transit_gateway.ec2_transit_gateway_id
     arn                         = module.transit_gateway.ec2_transit_gateway_arn
-    vpc_route_table_ids         = module.transit_gateway.ec2_transit_gateway_vpc_attachment_ids
     route_ids                   = module.transit_gateway.ec2_transit_gateway_route_ids
     route_table_id              = module.transit_gateway.ec2_transit_gateway_route_table_id
     route_table_association_ids = module.transit_gateway.ec2_transit_gateway_route_table_association_ids
     route_table_association     = module.transit_gateway.ec2_transit_gateway_route_table_association
+    route_table_propagation_ids = module.transit_gateway.ec2_transit_gateway_route_table_propagation_ids
     route_table_propagation     = module.transit_gateway.ec2_transit_gateway_route_table_propagation
-    vpc_attachments             = module.transit_gateway.ec2_transit_gateway_vpc_attachment_ids
+    vpc_attachment              = module.transit_gateway.ec2_transit_gateway_vpc_attachment
+    vpc_attachments_ids         = module.transit_gateway.ec2_transit_gateway_vpc_attachment_ids
     ram_share_id                = module.transit_gateway.ram_resource_share_id
   }
 }
