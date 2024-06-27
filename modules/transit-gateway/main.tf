1e357fa041b1686a1fc04d3966dd7dcca2e8fa4d
Cristian Beraha <cristian@cloudops.works>
2024-06-27 17:43:11 -0300

    fix: formatting

--- modules/transit-gateway/main.tf
+++ modules/transit-gateway/main.tf
@@ -70,7 +70,7 @@ resource "aws_ec2_tag" "this" {
 
 resource "aws_ec2_transit_gateway_vpc_attachment" "this" {
   depends_on = [aws_ram_resource_share_accepter.this]
-  for_each = var.vpc_attachments
+  for_each   = var.vpc_attachments
 
   transit_gateway_id = var.create_tgw ? aws_ec2_transit_gateway.this[0].id : each.value.tgw_id
   vpc_id             = each.value.vpc_id
