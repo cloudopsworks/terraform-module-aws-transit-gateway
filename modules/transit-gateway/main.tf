4cfe0dbb0e119576256045f0e583af7d39a0be09
Cristian Beraha <cristian@cloudops.works>
2024-07-03 23:02:04 -0300

    chore: transit_gateway_route_table_propagation parameter to avoid route table propagations

--- modules/transit-gateway/main.tf
+++ modules/transit-gateway/main.tf
@@ -152,7 +152,7 @@ resource "aws_ec2_transit_gateway_route_table_association" "this" {
 
 resource "aws_ec2_transit_gateway_route_table_propagation" "this" {
   for_each = {
-    for k, v in var.vpc_attachments : k => v if var.create_tgw && var.create_tgw_routes && try(v.transit_gateway_default_route_table_propagation, true) != true
+    for k, v in var.vpc_attachments : k => v if var.create_tgw && var.create_tgw_routes && try(v.transit_gateway_default_route_table_propagation, true) != true && try(v.transit_gateway_route_table_propagation, true) == true
   }
 
   # Create association if it was not set already by aws_ec2_transit_gateway_vpc_attachment resource
